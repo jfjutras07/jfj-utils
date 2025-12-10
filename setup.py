e7cadd779ac420893551e61a067e79a712a5d981
Jean-François Jutras <jfjutras07@hotmail.com>
2025-12-10 10:32:39 -0500

    Add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jfj-utils",
+    version="0.1.0",
+    description="A lightweight collection of Python utilities for analytics",
+    author="JF Jutras",
+    packages=find_packages(), 
+    python_requires=">=3.7",
+)
