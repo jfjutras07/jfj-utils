d3a23a4894fa2ad9542d1985bc09aa0ed37ab7e0
Jean-François Jutras <jfjutras07@hotmail.com>
2025-12-15 10:56:13 -0500

    Update and rename statistics_param.py to statistics_non_param.py

--- eda/statistics_non_param.py
+++ eda/statistics_non_param.py
@@ -1,35 +1,5 @@
 import pandas as pd
-from scipy.stats import skew, kurtosis
 from scipy.stats import mannwhitneyu
-
-#--- Function : numeric_skew_kurt ---
-def numeric_skew_kurt(df, numeric_cols):
-    """
-    Compute skewness and kurtosis for selected numeric columns.
-
-    Parameters:
-    - df: pandas DataFrame
-    - numeric_cols: list of numeric columns to analyze (must be explicitly provided)
-
-    Returns:
-    - skew_kurt_df: DataFrame with skewness and kurtosis for each column
-    """
-    results = []
-    
-    for col in numeric_cols:
-        if col not in df.columns:
-            print(f"Warning: {col} not found in DataFrame. Skipping.")
-            continue
-            
-        col_data = df[col].dropna()
-        results.append({
-            "Column": col,
-            "Skewness": skew(col_data),
-            "Kurtosis": kurtosis(col_data)
-        })
-    
-    skew_kurt_df = pd.DataFrame(results)
-    return skew_kurt_df
 
 #--- Function mann_whitney_cliff ---
 def mann_whitney_cliff(
