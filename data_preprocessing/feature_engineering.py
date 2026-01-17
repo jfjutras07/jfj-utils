d936de09d7101f18e9a77551d2d77d7924e351fe
Jean-François Jutras <jfjutras07@hotmail.com>
2026-01-17 09:10:45 -0500

    Update feature_engineering.py

--- data_preprocessing/feature_engineering.py
+++ data_preprocessing/feature_engineering.py
@@ -1,5 +1,31 @@
 import pandas as pd
 from sklearn.feature_selection import mutual_info_regression, mutual_info_classif
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from typing import Dict, List
+
+#--- Class : ratio_generator ---
+class ratio_generator(BaseEstimator, TransformerMixin):
+    """
+    Generates ratio features between pairs of columns.
+    Input format: {'NewFeatureName': ['NumeratorCol', 'DenominatorCol']}
+    """
+    def __init__(self, ratio_mappings: Dict[str, List[str]], fill_na_value: float = 0.0):
+        self.ratio_mappings = ratio_mappings
+        self.fill_na_value = fill_na_value
+
+    def fit(self, X: pd.DataFrame, y=None):
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X = X.copy()
+        for new_col, (num, den) in self.ratio_mappings.items():
+            # Replace 0 with NaN to ensure division results in NaN instead of Inf
+            den_safe = X[den].replace(0, np.nan)
+            X[new_col] = X[num] / den_safe
+            if self.fill_na_value is not None:
+                X[new_col] = X[new_col].fillna(self.fill_na_value)
+        return X
 
 # --- Function : mi_classification ---
 def mi_classification(X: pd.DataFrame, y: pd.Series) -> pd.Series:
